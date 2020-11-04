0e521a3ef0c536886c3b46f85a0c3a42e112d490
M1ndas <darbas.mindaugas@gmail.com>
2020-11-04 11:32:13 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,24 @@
 <?php
 
-// namespace Html;
-
 include './ns1/Table.php';
 include './ns2/Table.php';
 
-$t1 = new Html\Table(); 
-var_dump($t1);
+// ------------ 1 ------------
+// $t1 = new Html\Table(); 
+// var_dump($t1);
 
-$t2 = new Furniture\Table(); 
+// $t2 = new Furniture\Table(); 
+// var_dump($t2);
+
+// ------------ 2 ------------
+namespace Html;
+
+use Furniture as F;
+
+$t2 = new F\Table(); // naudosime Table klasę iš ns Furniture
 var_dump($t2);
+
+$t1 = new Table(); // naudosime Table klasę iš ns Html
+var_dump($t1);
 
 ?>
